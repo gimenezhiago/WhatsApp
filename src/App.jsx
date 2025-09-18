63e89e0a561d9731affa25ecb9f82f872753401c
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-09-17 23:55:10 -0300

    ret
    yt

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import * as C from './App.styles';
+import Title from './components/Title/Title';
+
+export default function App() {
+  return (
+    <C.Container>
+      <Title />
+    </C.Container>
+  )
+}
