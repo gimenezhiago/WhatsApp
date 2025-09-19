951508148ddd37afce720f5dbca9f67344608cc7
Bruno Pena <brunopena454@gmail.com>
2025-09-19 09:42:56 -0300

    Todos os caminhos adcionados
    Controller pronto

--- Back/server.js
+++ Back/server.js
@@ -1,9 +1,25 @@
 import express from "express";
+import * as controllerContacts from "./controller/contacts.controller.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//Busca por contatos
+app.get("/contacts", controllerContacts.getContacs);
+
+//Pesquisa por nome
+app.get("/contacts/:name", controllerContacts.getContactByName);
+
+//Cria novo contato
+app.post("/contacts/create", controllerContacts.createContact);
+
+//Deleta contato
+app.delete("/contacts/:id", controllerContacts.deleteContact);
+
+//Atualiza contato
+app.put("/contacts/:id", controllerContacts.updateContact);
 
 app.get("/", (req, res) => {
     res.send("🚀 Servidor rodando com Express + Nodemon!");
